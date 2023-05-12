6e8218bb7dd54c4db40446ee8cab6e2ee548f54c
Defkon1 <defkon1@gmail.com>
2023-05-12 13:51:45 +0200

    Added FiscalCodeHelper Added FiscalCodeHelper tests Added GitVersioning

---
+++ src/ItalianToolkit.Tests/People/FiscalCodeHelperTests.cs
@@ -0,0 +1,276 @@
+﻿using ItalianToolkit.People;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace ItalianToolkit.Tests.People
+{
+    internal class FiscalCodeHelperTests
+
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FiscalCodeShouldBeValidated()
+        {
+            {
+                var result = FiscalCodeHelper.IsFormallyValid("MRNLSS90H27A271J");
+
+                Assert.IsTrue(result);
+            }
+
+            {
+                var result = FiscalCodeHelper.IsFormallyValid("    XGNIUX23A07Z110V   ");
+
+                Assert.IsTrue(result);
+            }
+
+            {
+                var result = FiscalCodeHelper.IsFormallyValid("12345678901"); // temporary fiscal code -> 11 numeric chars
+
+                Assert.IsTrue(result);
+            }
+        }
+
+        [Test]
+        public void FiscalCodeShouldFailValidation()
+        {
+            {
+                var result = FiscalCodeHelper.IsFormallyValid("WrongStringAsFiscalCode");
+
+                Assert.IsFalse(result);
+            }
+
+            {
+                var result = FiscalCodeHelper.IsFormallyValid("XGN I U X2 3A07 Z110V");
+
+                Assert.IsFalse(result);
+            }
+
+            {
+                var result = FiscalCodeHelper.IsFormallyValid("123456789012345");
+
+                Assert.IsFalse(result);
+            }
+        }
+
+        [Test]
+        public void FiscalCodeValidationShouldThrow()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = FiscalCodeHelper.IsFormallyValid(string.Empty);
+            });
+        }
+
+        [Test]
+        public void FiscalCodeShouldBeCalculated()
+        {
+            // NOTE: The names and all the data used in this tests are fictitious.
+            //       No identification with actual persons (living or deceased),
+            //       places, buildings, and products is intended or should be inferred.
+
+            {
+                var assert = "MRNLSS90H27A271J";
+                var result = FiscalCodeHelper.CalculateFiscalCode("Marinelli", "Alessio", new DateTime(1990, 6, 27), "M", "A271");
+
+                TestContext.WriteLine($"TEST: {assert} - {result}");
+
+                Assert.AreEqual(assert, result);
+            }
+
+            {
+                var assert = "VRDGNN80A10E783B";
+                var result = FiscalCodeHelper.CalculateFiscalCode("Verdi", "Giovanni", new DateTime(1980, 1, 10), "M", "E783");
+
+                TestContext.WriteLine($"TEST: {assert} - {result}");
+
+                Assert.AreEqual(assert, result);
+            }
+
+            {
+                var assert = "RSSMRA69H45Z602F";
+                var result = FiscalCodeHelper.CalculateFiscalCode("Rossi", "Maria", new DateTime(1969, 6, 5), "F", "z602");
+
+                TestContext.WriteLine($"TEST: {assert} - {result}");
+
+                Assert.AreEqual(assert, result);
+            }
+
+            {
+                var assert = "BLUHUX64T01Z210I";
+                var result = FiscalCodeHelper.CalculateFiscalCode("BLU", "HU", new DateTime(1964, 12, 1), "M", "z210");
+
+                TestContext.WriteLine($"TEST: {assert} - {result}");
+
+                Assert.AreEqual(assert, result);
+            }
+
+            {
+                var assert = "MAXJIX17L51Z213S";
+                var result = FiscalCodeHelper.CalculateFiscalCode("MA", "JI", new DateTime(2017, 7, 11), "f", "Z213");
+
+                TestContext.WriteLine($"TEST: {assert} - {result}");
+
+                Assert.AreEqual(assert, result);
+            }
+
+            {
+                var assert = "XGNIUX23A07Z110V";
+                var result = FiscalCodeHelper.CalculateFiscalCode("XI GONG", "IU", new DateTime(2023, 1, 7), "m", "Z110");
+
+                TestContext.WriteLine($"TEST: {assert} - {result}");
+
+                Assert.AreEqual(assert, result);
+            }
+
+            {
+                var assert = "NRETMS80A01A271D";
+                var result = FiscalCodeHelper.CalculateFiscalCode("neri", "tommaso", new DateTime(1980, 1, 1), "m", "a271");
+
+                TestContext.WriteLine($"TEST: {assert} - {result}");
+
+                Assert.AreEqual(assert, result);
+            }
+        }
+
+        [Test]
+        public void FiscalCodeCalculationShouldThrow()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = FiscalCodeHelper.CalculateFiscalCode(string.Empty, "Alessio", new DateTime(1990, 6, 27), "M", "A271");
+            });
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = FiscalCodeHelper.CalculateFiscalCode(" ", "Alessio", new DateTime(1990, 6, 27), "M", "A271");
+            });
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+
+                var result = FiscalCodeHelper.CalculateFiscalCode("Marinelli", " ", new DateTime(1990, 6, 27), "M", "A271");
+            });
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = FiscalCodeHelper.CalculateFiscalCode("Marinelli", "Alessio", new DateTime(1990, 6, 27), "", "A271");
+            });
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = FiscalCodeHelper.CalculateFiscalCode("Marinelli", "Alessio", new DateTime(1990, 6, 27), "AnyOtherValue", "A271");
+            });
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = FiscalCodeHelper.CalculateFiscalCode("Marinelli", "Alessio", new DateTime(1990, 6, 27), "M", string.Empty);
+            });
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = FiscalCodeHelper.CalculateFiscalCode("Marinelli", "Alessio", new DateTime(1990, 6, 27), "M", "WrongBelfioreFormat");
+            });
+        }
+
+        [Test]
+        public void HomocodiesShouldBeCalculated()
+        {
+            {
+                var result = FiscalCodeHelper.GenerateHomocodies("MRNLSS90H27A271J");
+
+                Assert.IsNotNull(result);
+
+                var expected = new List<string>() {
+                    "MRNLSS90H27A27MB",
+                    "MRNLSS90H27A2TMN",
+                    "MRNLSS90H27ANTMC",
+                    "MRNLSS90H2TANTMZ",
+                    "MRNLSS90HNTANTMK",
+                    "MRNLSS9LHNTANTMV",
+                    "MRNLSSVLHNTANTMK",
+                    "MRNLSS90H27A27MB",
+                    "MRNLSS90H27A2T1V",
+                    "MRNLSS90H27AN71Y",
+                    "MRNLSS90H2TA271G",
+                    "MRNLSS90HN7A271U",
+                    "MRNLSS9LH27A271U",
+                    "MRNLSSV0H27A271Y"
+                };
+
+                Assert.AreEqual(expected.Count, result.Count);
+
+                expected.ForEach(x => { Assert.IsTrue(result.Contains(x)); });
+            }
+        }
+
+        [Test]
+        public void HomocodiesCalculationShouldThrow()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = FiscalCodeHelper.GenerateHomocodies(string.Empty);
+            });
+        }
+
+        [Test]
+        public void HomocodiesShouldFailCalculation()
+        {
+            {
+                var result = FiscalCodeHelper.GenerateHomocodies("WrongString");
+
+                Assert.IsNotNull(result);
+                Assert.AreEqual(0, result.Count);
+            }
+
+            {
+                var result = FiscalCodeHelper.GenerateHomocodies("12345678901");  // temporary fiscal code
+
+                Assert.IsNotNull(result);
+                Assert.AreEqual(0, result.Count);
+            }
+        }
+
+        [Test]
+        public void HomocodyShouldBeValidated()
+        {
+            {
+                var expected = new List<string>() {
+                    "MRNLSS90H27A27MB",
+                    "MRNLSS90H27A2TMN",
+                    "MRNLSS90H27ANTMC",
+                    "MRNLSS90H2TANTMZ",
+                    "MRNLSS90HNTANTMK",
+                    "MRNLSS9LHNTANTMV",
+                    "MRNLSSVLHNTANTMK",
+                    "MRNLSS90H27A27MB",
+                    "MRNLSS90H27A2T1V",
+                    "MRNLSS90H27AN71Y",
+                    "MRNLSS90H2TA271G",
+                    "MRNLSS90HN7A271U",
+                    "MRNLSS9LH27A271U",
+                    "MRNLSSV0H27A271Y"
+                };
+
+                expected.ForEach(x => Assert.IsTrue(FiscalCodeHelper.IsHomocody("MRNLSS90H27A271J", x)));
+            }
+        }
+
+        [Test]
+        public void HomocodyShouldFailValidation()
+        {
+            {
+                var expected = new List<string>() {
+                    "WrongString",
+                    "12345678901"
+                };
+
+                expected.ForEach(x => Assert.IsFalse(FiscalCodeHelper.IsHomocody("MRNLSS90H27A271J", x)));
+            }
+        }
+    }
+}
---
+++ src/ItalianToolkit/People/FiscalCodeHelper.cs
@@ -0,0 +1,473 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ItalianToolkit.People
+{
+    public static class FiscalCodeHelper
+    {
+        private static readonly Dictionary<int, char> _homocodyMap = new Dictionary<int, char>()
+        {
+            {0, 'L'},
+            {1, 'M'},
+            {2, 'N'},
+            {3, 'P'},
+            {4, 'Q'},
+            {5, 'R'},
+            {6, 'S'},
+            {7, 'T'},
+            {8, 'U'},
+            {9, 'V'}
+        };
+
+        private static readonly Dictionary<int, char> _monthsMap = new Dictionary<int, char>()
+        {
+            {1, 'A'},
+            {2, 'B'},
+            {3, 'C'},
+            {4, 'D'},
+            {5, 'E'},
+            {6, 'H'},
+            {7, 'L'},
+            {8, 'M'},
+            {9, 'P'},
+            {10, 'R'},
+            {11, 'S'},
+            {12, 'T'}
+        };
+
+        private static readonly Dictionary<char, int> _controlEvenCharacters = new Dictionary<char, int>()
+        {
+            {'A', 0 },
+            {'B', 1 },
+            {'C', 2 },
+            {'D', 3 },
+            {'E', 4 },
+            {'F', 5 },
+            {'G', 6 },
+            {'H', 7 },
+            {'I', 8 },
+            {'J', 9 },
+            {'K', 10 },
+            {'L', 11 },
+            {'M', 12 },
+            {'N', 13 },
+            {'O', 14 },
+            {'P', 15 },
+            {'Q', 16 },
+            {'R', 17 },
+            {'S', 18 },
+            {'T', 19 },
+            {'U', 20 },
+            {'V', 21 },
+            {'W', 22 },
+            {'X', 23 },
+            {'Y', 24 },
+            {'Z', 25 },
+            {'0', 0 },
+            {'1', 1 },
+            {'2', 2 },
+            {'3', 3 },
+            {'4', 4 },
+            {'5', 5 },
+            {'6', 6 },
+            {'7', 7 },
+            {'8', 8 },
+            {'9', 9 }
+        };
+
+        private static readonly Dictionary<char, int> _controlOddCharacters = new Dictionary<char, int>()
+        {
+            {'A', 1 },
+            {'B', 0 },
+            {'C', 5 },
+            {'D', 7 },
+            {'E', 9 },
+            {'F', 13 },
+            {'G', 15 },
+            {'H', 17 },
+            {'I', 19 },
+            {'J', 21 },
+            {'K', 2 },
+            {'L', 4 },
+            {'M', 18 },
+            {'N', 20 },
+            {'O', 11 },
+            {'P', 3 },
+            {'Q', 6 },
+            {'R', 8 },
+            {'S', 12 },
+            {'T', 14 },
+            {'U', 16 },
+            {'V', 10 },
+            {'W', 22 },
+            {'X', 25 },
+            {'Y', 24 },
+            {'Z', 23 },
+            {'0', 1 },
+            {'1', 0 },
+            {'2', 5 },
+            {'3', 7 },
+            {'4', 9 },
+            {'5', 13 },
+            {'6', 15 },
+            {'7', 17 },
+            {'8', 19 },
+            {'9', 21 }
+        };
+
+        private static readonly Dictionary<int, char> _controlDigit = new Dictionary<int, char>()
+        {
+            { 0, 'A' },
+            { 1, 'B' },
+            { 2, 'C' },
+            { 3, 'D' },
+            { 4, 'E' },
+            { 5, 'F' },
+            { 6, 'G' },
+            { 7, 'H' },
+            { 8, 'I' },
+            { 9, 'J' },
+            { 10, 'K' },
+            { 11, 'L' },
+            { 12, 'M' },
+            { 13, 'N' },
+            { 14, 'O' },
+            { 15, 'P' },
+            { 16, 'Q' },
+            { 17, 'R' },
+            { 18, 'S' },
+            { 19, 'T' },
+            { 20, 'U' },
+            { 21, 'V' },
+            { 22, 'W' },
+            { 23, 'X' },
+            { 24, 'Y' },
+            { 25, 'Z' }
+        };
+
+        public static bool IsFormallyValid(string fiscalCode)
+        {
+            if (string.IsNullOrWhiteSpace(fiscalCode))
+            {
+                throw new ArgumentException(nameof(fiscalCode));
+            }
+
+            fiscalCode = fiscalCode.Trim();
+
+            if (fiscalCode.Length != 16 && fiscalCode.Length != 11)
+            {
+                return false;
+            }
+
+            string pattern = "^([A-Z]{6}[0-9LMNPQRSTUV]{2}[ABCDEHLMPRST]{1}[0-9LMNPQRSTUV]{2}[A-Z]{1}[0-9LMNPQRSTUV]{3}[A-Z]{1})$|([0-9]{11})$";
+            var rgx = new Regex(pattern);
+
+            return rgx.IsMatch(fiscalCode.ToUpper());
+        }
+
+        /// <inheritdoc />
+        public static string CalculateFiscalCode(string lastName, string firstName, DateTime birthDate, string gender, string cityCode)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentException(nameof(lastName));
+            }
+
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentException(nameof(firstName));
+            }
+
+            var acceptedGenders = new string[] { "M", "F" };
+            if (string.IsNullOrWhiteSpace(gender) || !acceptedGenders.Contains(gender.ToUpper(), StringComparer.InvariantCultureIgnoreCase))
+            {
+                throw new ArgumentException(nameof(gender));
+            }
+
+            var codePattern = "^[A-Za-z][0-9][0-9][0-9]$";
+            var rgx = new Regex(codePattern);
+            if (string.IsNullOrWhiteSpace(cityCode) || !rgx.IsMatch(cityCode))
+            {
+                throw new ArgumentException(nameof(cityCode));
+            }
+
+            string fiscalCode;
+
+            try
+            {
+                firstName = firstName.ToUpper();
+                lastName = lastName.ToUpper();
+                gender = gender.ToUpper();
+                cityCode = cityCode.ToUpper();
+
+                fiscalCode = $"{ParseLastName(lastName)}{ParseFirstName(firstName)}{ParseBirthDate(birthDate, gender)}{cityCode}";
+                fiscalCode += CalculateControlDigit(fiscalCode);
+            }
+            catch (Exception)
+            {
+                fiscalCode = string.Empty;
+            }
+
+            return fiscalCode;
+        }
+
+        /// <inheritdoc />
+        public static bool IsHomocody(string baseFiscalCode, string homocodyFiscalCode)
+        {
+            var variations = GenerateHomocodies(baseFiscalCode.ToUpper());
+
+            return variations.Contains(homocodyFiscalCode.ToUpper());
+        }
+
+        /// <summary>
+        /// Generate all valid homocodies from a base fiscal code
+        /// </summary>
+        /// <param name="baseFiscalCode">the base fiscal code</param>
+        /// <returns>a list with all the valid homocodies</returns>
+        public static List<string> GenerateHomocodies(string baseFiscalCode)
+        {
+            if (string.IsNullOrWhiteSpace(baseFiscalCode))
+            {
+                throw new ArgumentException(nameof(baseFiscalCode));
+            }
+
+            var variations = new List<string>();
+            var recalculatedVariations = new List<string>();
+
+            // not a standard fiscal code
+            if (baseFiscalCode.Length != 16)
+            {
+                return variations;
+            }
+
+            baseFiscalCode = baseFiscalCode.ToUpper();
+
+            if (!IsFormallyValid(baseFiscalCode))
+            {
+                return variations;
+            }
+
+            // sequential - multiple
+            for (int i = 1; i <= 7; i++)
+            {
+                var variation = string.Empty;
+
+                var mappedChars = 0;
+
+                for (int j = baseFiscalCode.Length - 1; j >= 0; j--)
+                {
+                    char computingChar = baseFiscalCode[j];
+
+                    if (!char.IsDigit(computingChar) || mappedChars == i)
+                    {
+                        variation = variation.Insert(0, computingChar.ToString());
+                    }
+                    else
+                    {
+                        int lookup = Convert.ToInt32(computingChar.ToString());
+                        var mappedChar = _homocodyMap[lookup];
+
+                        variation = variation.Insert(0, mappedChar.ToString());
+
+                        mappedChars++;
+                    }
+                }
+
+                variations.Add(variation);
+            }
+
+            // single characters variation
+            foreach (int pos in new[] { 14, 13, 12, 10, 9, 7, 6 })
+            {
+                int lookup = Convert.ToInt32(baseFiscalCode[pos].ToString());
+
+                var variation = baseFiscalCode.Remove(pos, 1);
+                variation = variation.Insert(pos, _homocodyMap[lookup].ToString());
+
+                variations.Add(variation);
+            }
+
+            // recalculate control digits
+            foreach (var v in variations)
+            {
+                char controlDigit = CalculateControlDigit(v);
+
+                var recalculatedVariation = v.Substring(0, 15).Insert(15, controlDigit.ToString());
+
+                recalculatedVariations.Add(recalculatedVariation);
+            }
+
+            return recalculatedVariations;
+        }
+
+        private static char CalculateControlDigit(string fiscalCode)
+        {
+            if (fiscalCode.Length >= 16)
+            {
+                fiscalCode = fiscalCode.Substring(0, 15);
+            }
+
+            int totalSum = 0;
+            for (int i = 0; i < fiscalCode.Length; i++)
+            {
+                totalSum += (i + 1) % 2 == 0
+                    ? _controlEvenCharacters[fiscalCode[i]]
+                    : _controlOddCharacters[fiscalCode[i]];
+            }
+
+            int control = totalSum % 26;
+
+            return _controlDigit[control];
+        }
+
+        private static string ParseLastName(string lastName)
+        {
+            StringBuilder result = new StringBuilder();
+            int Cont = 0;
+
+            try
+            {
+                // take consonants
+                for (int i = 0; i <= lastName.Length - 1; i++)
+                {
+                    if ((IsVowel(lastName[i]) == false) & (lastName[i] != ' ') && (lastName[i] != '\'') && (lastName[i] != '-') && (lastName[i] != '_'))
+                    {
+                        result.Append(lastName[i]);
+                        Cont += 1;
+                        if (Cont == 3)
+                            return result.ToString();
+                    }
+                }
+
+                // take vowels
+                if (Cont < 3)
+                {
+                    for (int i = 0; i <= lastName.Length - 1; i++)
+                    {
+                        if ((IsVowel(lastName[i]) == true) && (lastName[i] != ' ') && (lastName[i] != '\'') && (lastName[i] != '-') && (lastName[i] != '_'))
+                        {
+                            result.Append(lastName[i]);
+                            Cont += 1;
+                            if (Cont == 3)
+                                return result.ToString();
+                        }
+                    }
+                }
+
+                // fill out missing chars
+                for (int i = 1; i <= 3 - Cont; i++)
+                    result.Append("X");
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+
+            return result.ToString();
+        }
+
+        private static string ParseFirstName(string firstName)
+        {
+            StringBuilder result = new StringBuilder();
+            int Cont = 0;
+
+            try
+            {
+                // take consonants 1 3 4
+                for (int i = 0; i <= firstName.Length - 1; i++)
+                {
+                    if ((IsVowel(firstName[i]) == false) && (firstName[i] != ' ') && (firstName[i] != '\'') && (firstName[i] != '-') && (firstName[i] != '_'))
+                    {
+                        if (Cont != 1)
+                            result.Append(firstName[i]);
+                        Cont += 1;
+                        if (Cont == 4)
+                            return result.ToString(); ;
+                    }
+                }
+
+                if (result.Length < 3)
+                {
+                    result.Remove(0, result.Length);
+                    Cont = 0;
+                    // take all consonants
+                    for (int i = 0; i <= firstName.Length - 1; i++)
+                    {
+                        if ((IsVowel(firstName[i]) == false) & (firstName[i] != ' ') & (firstName[i] != '\'') & (firstName[i] != '-') & (firstName[i] != '_'))
+                        {
+                            result.Append(firstName[i]);
+                            Cont += 1;
+                            if (Cont == 3)
+                                return result.ToString();
+                        }
+                    }
+                }
+
+                // take vowels
+                if (Cont < 3)
+                {
+                    for (int i = 0; i <= firstName.Length - 1; i++)
+                    {
+                        if ((IsVowel(firstName[i]) == true) & (firstName[i] != ' ') & (firstName[i] != '\'') & (firstName[i] != '-') & (firstName[i] != '_'))
+                        {
+                            result.Append(firstName[i]);
+                            Cont += 1;
+                            if (Cont == 3)
+                                return result.ToString();
+                        }
+                    }
+                }
+
+                // fill out missing chars
+                for (int i = 1; i <= 3 - Cont; i++)
+                    result.Append("X");
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+
+            return result.ToString();
+        }
+
+        private static string ParseBirthDate(DateTime birthDate, string gender)
+        {
+            StringBuilder result = new StringBuilder();
+
+            string year;
+            string day;
+
+            year = birthDate.ToString("yy");
+
+            result.Append(year);
+            result.Append(DecodeMonth(birthDate.Month));
+
+            if (gender == "F")
+            {
+                day = (birthDate.Day + 40).ToString();
+            }
+            else
+            {
+                day = birthDate.Day.ToString("D2");
+            }
+
+            result.Append(day);
+            return result.ToString();
+        }
+
+        private static string DecodeMonth(int month)
+        {
+            return _monthsMap[month].ToString();
+        }
+
+        private static bool IsVowel(char letter)
+        {
+            char[] vowels = new char[] { 'A', 'E', 'I', 'O', 'U' };
+
+            return vowels.Contains(char.ToUpper(letter));
+        }
+    }
+}
